5898134b056d34f0f564c0d35b2bbeb033169dae
Justin Klein <JustinKln51@gmail.com>
2021-02-24 10:15:35 +0100

    Search function was implemented

--- detail.js
+++ detail.js
@@ -6,3 +6,15 @@ const searchButton = document.getElementById('search-button')
 const picture = document.getElementById('avatar')
 const nameField = document.getElementById('name')
 const card = document.getElementById('card')
+
+const getPokemon = (num) => {
+  
+    fetch(`https://pokeapi.co/api/v2/pokemon/${num}`)
+      .then((res) => res.json())
+      .then((data) => {
+        picture.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`;
+        nameField.innerHTML = data.name;
+      });
+  };
+
+searchButton.addEventListener("click", () => getPokemon(searchInput.value));
