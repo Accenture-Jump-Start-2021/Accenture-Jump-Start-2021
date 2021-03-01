04bbeba71833c6814ea7f5e1dbdc049773e422fd
Martin Jaskulla <martin.jaskulla@accenture.com>
2021-03-01 11:35:57 +0100

    test

--- capitalise.js
+++ capitalise.js
@@ -1,3 +1,13 @@
 export function capitalise(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
+
+
+
+
+  test(() => {
+
+    const result = capitalise('pikachu')
+    expect(result).toBe('Pikachu')
+
+  })
